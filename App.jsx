cc2fcaf7654bacb311cae2897e5f0f97780fee77
Ray5mz <136928230+Ray5mz@users.noreply.github.com>
2025-04-12 00:51:10 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import GrayButton from './components/GrayButton';
+import EmergencyButton from './components/EmergencyButton';
+import PurpleContainer from './components/PurpleContainer';
+import Login from './components/Login';
+import Header from './components/Header';
+function App() {
+  return (
+    <>
+     <Header></Header>
+    </>
+  );
+}
+
+export default App;
