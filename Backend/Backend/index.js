a7d67966e636b5b89841b9cab36e109a23e0e7a4
Ray5mz <136928230+Ray5mz@users.noreply.github.com>
2025-04-12 11:15:19 +0100

    Add files via upload

--- Backend/Backend/index.js
+++ Backend/Backend/index.js
@@ -1,1 +1,21 @@
-
+import express, { json } from "express"
+import cors from "cors";
+import dotenv from "dotenv";
+import authent from "./routes/authRouter.js";
+import { connectDb } from "./lib/db.js";
+import cookieParser from "cookie-parser"
+import publi from "./routes/publRouter.js";
+import chat from "./routes/chatAi.js"
+dotenv.config()
+const app = express();
+const Port = process.env.Port;
+app.use(express.json());
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use("/auth" , authent)
+app.use("/" , publi);
+app.use("/ai-chat" , chat)
+app.listen(5000 ,()=>{
+    connectDb()
+console.log("server connected on http://localhost:"+Port)
+})
