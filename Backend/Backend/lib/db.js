ddd72d98bfa7b025edab2f85db9dcd71e3ae1ad2
Ray5mz <136928230+Ray5mz@users.noreply.github.com>
2025-04-12 11:16:53 +0100

    Add files via upload

--- Backend/Backend/lib/db.js
+++ Backend/Backend/lib/db.js
@@ -1,1 +1,9 @@
-
+import mongoose from "mongoose";
+export const connectDb = async ()=>{
+    try{
+const conn = await mongoose.connect(process.env.MONGODB_URI)
+console.log(`dataBase connected : ${conn.connection.host}`)
+    } catch(err){
+console.log("mongoDb error : "+ err)
+    }
+}
